6f96459e9f3d0b5d5d9ff9e4f33a707b84f65b30
Zayaan Rahman <69658517+zayaanra@users.noreply.github.com>
2021-05-22 15:38:24 -0400

    Added functionality for GUI

--- gui/window.py
+++ gui/window.py
@@ -1,5 +1,24 @@
 from tkinter import *
+
+# Initialize root
 root = Tk()
-lb = Label(root, text="Running a test...")
-lb.pack()
+root.title("Sorting Visualizer")
+root.maxsize(2000, 1000)
+root.config(bg="white")
+
+selected = StringVar()
+
+# Create a toolbar at the top
+bar = Frame(root, bg='black')
+
+# List of various sorting algorithms
+options = ["Bubble Sort", "Merge Sort", "Quick Sort"]
+selected.set(options[0])
+
+# Create dropdown menu for algorithm selection
+dropdown = OptionMenu(bar, selected, *options)
+dropdown.pack(side=LEFT, padx=3, pady=3)
+
+bar.pack(side=TOP, fill=X)
+
 root.mainloop()
