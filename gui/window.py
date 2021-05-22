094f24fe9ece49586ba2555b13bd853698058f2b
Zayaan Rahman <69658517+zayaanra@users.noreply.github.com>
2021-05-22 19:45:12 -0400

    Added a dropdown menu, two buttons, and a slider

--- gui/window.py
+++ gui/window.py
@@ -3,21 +3,46 @@ from tkinter import *
 # Initialize root
 root = Tk()
 root.title("Sorting Visualizer")
-root.maxsize(2000, 1000)
+# Set maximum window size
+root.maxsize(1500, 1000)
+
+# Set window size when application is first opened
+root.geometry("1500x1000")
+
 root.config(bg="white")
 
+# Used for dropdown menus
 selected = StringVar()
+color = StringVar()
 
 # Create a toolbar at the top
 bar = Frame(root, bg='black')
 
-# List of various sorting algorithms
+# Set variables and other data
 options = ["Bubble Sort", "Merge Sort", "Quick Sort"]
 selected.set(options[0])
+colors = ["Blue", "Red", "Yellow", "Green", "Orange", "Pink", "Purple"]
+color.set(colors[0])
 
-# Create dropdown menu for algorithm selection
+# Create label and dropdown menu for algorithm selection
 dropdown = OptionMenu(bar, selected, *options)
 dropdown.pack(side=LEFT, padx=3, pady=3)
+
+# Create a slider to determine input size
+slider = Scale(bar, label="Input Size", from_=0, to=100, length=400, resolution=1, orient=HORIZONTAL)
+slider.pack(side=LEFT, padx=3, pady=3)
+
+# Create a second dropdown menu to allow user to customize color of visualization
+customizable = OptionMenu(bar, color, *colors)
+customizable.pack(side=LEFT, padx=3, pady=3)
+
+# Create a button to start the visualization
+start = Button(bar, text="Start")
+start.pack(side=LEFT, padx=3, pady=3)
+
+# Create a button to pause the visualization
+pause = Button(bar, text="Pause")
+pause.pack(side=LEFT, padx=3, pady=3)
 
 bar.pack(side=TOP, fill=X)
 
