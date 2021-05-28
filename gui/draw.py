7ca231dcf9c15b360030cdb56ed3eb8cb8b1d4c0
Zayaan Rahman <69658517+zayaanra@users.noreply.github.com>
2021-05-28 18:40:11 -0400

    Added functionality to draw out dataset

--- gui/draw.py
+++ gui/draw.py
@@ -1,6 +1,17 @@
-def draw(data):
-    return -1
+from tkinter import *
 
 
-def change_color():
-    return -1
+def draw(data, color):
+    # Create a canvas
+    canvas = Canvas(width=2000, height=930, bg="grey")
+    # Offsets used to determine the distance away from each bar
+    x1_offset = 50
+    x2_offset = 75
+    # Loop through randomly generated dataset and draw each number out as a bar
+    for i in data:
+        # TODO - Need to figure out to correctly place each rectangle next to each other, no padding should exist
+        # TODO - Height of each rectangle should change depending on the number it represents
+        canvas.create_rectangle(x1_offset, 0, x2_offset, 500, fill="red")
+        x1_offset += 50
+        x2_offset += 50
+    return canvas
