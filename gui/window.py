7c53a0e8c91becba11c907c65c6c146d7475be5c
Zayaan Rahman <69658517+zayaanra@users.noreply.github.com>
2021-05-30 15:15:21 -0400

    Deleted draw.py and implemented it into window.py and fixed draw()

--- gui/window.py
+++ gui/window.py
@@ -1,6 +1,28 @@
 from random import randint
 from tkinter import *
-from gui.draw import draw
+
+
+# Event-Handlers
+
+# Draws data set out every time the "Generate" button is pressed
+def draw():
+    # Generate random data dependent on the user input size
+    length = slider.get()
+    data = [i for i in range(1, length+1)]
+    canvas.delete("all")
+    # Offsets used to determine the distance away from each bar
+    x1_offset = 50
+    x2_offset = 75
+    # Loop through randomly generated dataset and draw each number out as a bar
+    for i in data:
+        # TODO - Need to figure out to correctly place each rectangle next to each other, no padding should exist
+        # TODO - Height of each rectangle should change depending on the number it represents
+        # TODO - Make rectangles thinner to fit in more?
+        canvas.create_rectangle(x1_offset, 0, x2_offset, 500, fill="blue")
+        x1_offset += 50
+        x2_offset += 50
+    canvas.pack(side=BOTTOM)
+
 
 # Initialize root
 root = Tk()
@@ -31,7 +53,7 @@ dropdown = OptionMenu(bar, selected, *options)
 dropdown.pack(side=LEFT, padx=3, pady=3)
 
 # Create a slider to determine input size
-slider = Scale(bar, label="Input Size", from_=0, to=100, length=400, resolution=1, orient=HORIZONTAL)
+slider = Scale(bar, label="Input Size", from_=1, to=100, length=400, resolution=1, orient=HORIZONTAL)
 slider.pack(side=LEFT, padx=3, pady=3)
 
 # Create a second dropdown menu to allow user to customize color of visualization
@@ -46,14 +68,12 @@ start.pack(side=LEFT, padx=3, pady=3)
 pause = Button(bar, text="Pause")
 pause.pack(side=LEFT, padx=3, pady=3)
 
-# Create a button to generate a random data set
-generate = Button(bar, text="Generate")
-generate.pack(side=LEFT, padx=3, pady=3)
+# Create a canvas
+canvas = Canvas(width=2000, height=930, bg="grey")
 
-# Generate data set using the user input size
-random_data = [i for i in range(randint(1, 50))]
-c = draw(random_data, "blue")  # TODO - should only draw a new data set every time the user clicks "Generate"
-c.pack(side=BOTTOM)
+# Create a button to generate a random data set
+generate = Button(bar, text="Generate", command=draw)
+generate.pack(side=LEFT, padx=3, pady=3)
 
 bar.pack(side=TOP, fill=X)
 
