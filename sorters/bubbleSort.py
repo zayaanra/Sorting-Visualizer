0d1194489ea90054a5adf0234b67d9ef21f6a619
Zayaan Rahman <69658517+zayaanra@users.noreply.github.com>
2021-06-07 16:39:55 -0400

    Removed swap

--- sorters/bubbleSort.py
+++ sorters/bubbleSort.py
@@ -1,15 +1,8 @@
-def swap(lst, idx1, idx2):
-    tmp = lst[idx1]
-    lst[idx1] = lst[idx2]
-    lst[idx2] = tmp
-
-
+# lst - data to sort
+# bars - used to animate the sort
 def bubbleSort(lst):
     n = len(lst)
     for i in range(0, n - 1):
         for j in range(0, n - i - 1):
             if lst[j] > lst[j + 1]:
-                swap(lst, j + 1, j)
-
-
-
+                lst[j+1], lst[j] = lst[j], lst[j+1]
