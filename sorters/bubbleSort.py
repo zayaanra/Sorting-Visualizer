ac3a12404f246e2bc862e793dd9190b800099158
Zayaan Rahman <69658517+zayaanra@users.noreply.github.com>
2021-06-10 16:56:15 -0400

    Added call to animate()

--- sorters/bubbleSort.py
+++ sorters/bubbleSort.py
@@ -1,3 +1,6 @@
+import gui.window
+
+
 # lst - data to sort
 # bars - used to animate the sort
 def bubbleSort(lst):
@@ -5,4 +8,5 @@ def bubbleSort(lst):
     for i in range(0, n - 1):
         for j in range(0, n - i - 1):
             if lst[j] > lst[j + 1]:
-                lst[j+1], lst[j] = lst[j], lst[j+1]
+                lst[j + 1], lst[j] = lst[j], lst[j + 1]
+                gui.window.animate(lst[j][1], lst[j + 1][1])
